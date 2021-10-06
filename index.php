38d775d8a7eaedd8705cb16fdc32e0945fc497c4
Kaarel Kallas <kaarel.kallas@ametikool.ee>
2021-10-06 10:29:04 +0300

    1

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'config.php';
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+$stmt = $pdo->query('SELECT * FROM books');
+while ($row = $stmt->fetch())
+{
+    echo '<a href="book.php?id=' . $row['id'] . '">' . $row['title'] . '</a>' . '<br>';
+}
